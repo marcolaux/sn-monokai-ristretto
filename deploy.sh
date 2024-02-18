00d595d5ad8529406378e7b9dd3fa4fe4613a22f
marcaux <contact@marcaux.de>
2024-02-18 13:53:13 +0100

    0.4

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source .env
 zip -r sn-monokai-ristretto.zip ./ -x ".env" "*.sh" ".git*" ".DS*"
-rsync -arvz -e ssh --progress --delete --exclude={'.env','*.sh','.git*','.DS*'} ./ $server:$path
+rsync -arvz -e ssh --progress --delete --exclude={'.env','*.sh','.git*','.DS*','.htaccess'} ./ $server:$path
