850745d9ff018ebd6c9c968eb742ed091328ddfc
marcaux <contact@marcaux.de>
2024-02-24 11:25:27 +0100

    update to scss

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 source .env
-zip -r sn-monokai-ristretto.zip ./ -x ".env" "*.sh" ".git*" ".DS*"
-rsync -arvz -e ssh --progress --delete --exclude={'.env','*.sh','.git*','.DS*','.htaccess'} ./ $server:$path
+rm sn-monokai-ristretto.zip
+zip -r sn-monokai-ristretto.zip ./ -x "package-lock.json" ".env" "*.sh" ".git*" ".DS*" ".htaccess" "node_modules/*" "src/*" "*.txt"
+rsync -arvz -e ssh --progress --delete --exclude={'.env','*.sh','.git*','.DS*','.htaccess','node_modules','src','.gitkeep','*.txt'} ./ $server:$path
