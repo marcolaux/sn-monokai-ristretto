c068e36f7687d80abb2597f1fc1c7ad7e3218545
marcaux <contact@marcaux.de>
2024-02-24 12:38:18 +0100

    lexical code formatting

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 source .env
 rm sn-monokai-ristretto.zip
-zip -r sn-monokai-ristretto.zip ./ -x "package-lock.json" ".env" "*.sh" ".git*" ".DS*" ".htaccess" "node_modules/*" "src/*" "*.txt"
-rsync -arvz -e ssh --progress --delete --exclude={'.env','*.sh','.git*','.DS*','.htaccess','node_modules','src','.gitkeep','*.txt'} ./ $server:$path
+zip -r sn-monokai-ristretto.zip ./ -x "package-lock.json" ".env" "*.sh" ".git*" ".git/*" "dist/.gitkeep" ".DS*" ".htaccess" "node_modules/*" "src/*" "*.txt"
+ssh $server "cd $path; find ./ -type f -not -name '.htaccess' -delete; rm -fr dist"
+scp sn-monokai-ristretto.zip $server:$path/sn-monokai-ristretto.zip
+ssh $server "cd $path; unzip sn-monokai-ristretto.zip; rm sn-monokai-ristretto.zip"
