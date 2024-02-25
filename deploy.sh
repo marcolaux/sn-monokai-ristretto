e9099b463887a0213284de036977343d3d54dbdb
marcaux <contact@marcaux.de>
2024-02-25 11:41:08 +0100

    fixed ext.json, super formatting, note selection checkbox

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rm sn-monokai-ristretto.zip
 zip -r sn-monokai-ristretto.zip ./ -x "package-lock.json" ".env" "*.sh" ".git*" ".git/*" "dist/.gitkeep" ".DS*" ".htaccess" "node_modules/*" "src/*" "*.txt"
 ssh $server "cd $path; find ./ -type f -not -name '.htaccess' -delete; rm -fr dist"
 scp sn-monokai-ristretto.zip $server:$path/sn-monokai-ristretto.zip
-ssh $server "cd $path; unzip sn-monokai-ristretto.zip; rm sn-monokai-ristretto.zip"
+ssh $server "cd $path; unzip sn-monokai-ristretto.zip"
